6d6299c6802ae09ebb12fdc2620876a468eab700
propatcher <dfyz240206@gmail.com>
2025-11-17 17:11:27 +0400

    models

--- app/infra/database/models.py
+++ app/infra/database/models.py
@@ -6,13 +6,17 @@ class LinkModel(DeclarativeBase):
    short_code: Mapped[str] = mapped_column(String(12), unique=True),
    user_id: Mapped[int]= mapped_column(),
    click_count: Mapped[int] = mapped_column()
+   users : Mapped[list[User]] = relationship("User", back_populates="link")
 
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}')>"
 
 class UserModel(DeclarativeBase):
     id : Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
-    login : Mapped[str] = mapped_column(String(40))
-    email : Mapped[str] = mapped_column(String())
-    hashed_password : Mapped[str]
-    created_at : datetime
+    login : Mapped[str] = mapped_column(String(255))
+    email : Mapped[str] = mapped_column(String(255))
+    hashed_password : Mapped[str] = mapped_column(String(255))
+    created_at : Mapped[datetime] = mapped_column(String(255))
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}')>"
